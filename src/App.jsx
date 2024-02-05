cffceae156b9bfd76f9577a2c1042148e65cdc58
Andres Santamarina <andresesantamarina@gmail.com>
2024-02-05 10:22:59 -0300

    importe el logo en el componente imagen en vez de escribir la ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./assets/logoSimpsons.png"
 import Frase from "./components/Frase";
 import { Image, Container, Button } from "react-bootstrap";
 
 function App() {
   return (
     <Container className="text-center my-4">
-      <Image src="./src/assets/logoSimpsons.png" fluid className="logoSimpsons" />
+      <Image src={logo} fluid className="logoSimpsons" />
       <Frase />
       <Button className="mb-3" variant="warning">Obtener frase</Button>{" "}
     </Container>
