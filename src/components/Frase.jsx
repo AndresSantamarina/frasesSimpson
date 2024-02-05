4959ae26f1310a4ed4da4cab3acba7593db24dbc
Andres Santamarina <andresesantamarina@gmail.com>
2024-02-05 10:27:31 -0300

    agregue el alt a la imagen del bart y la importe

--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,11 +1,12 @@
 import Image from "react-bootstrap/Image";
+import bart from "../assets/bart.png"
 import { Container } from "react-bootstrap";
 
 const Frase = () => {
   return (
     <Container className="my-3">
       <h2>Bart Simpson</h2>
-      <Image src="src/assets/bart.png" fluid className="bart" />
+      <Image src={bart} alt="Imagen de Bart" fluid className="bart" />
       <p className="text-center my-3">Ay Caramba!</p>
     </Container>
   );
