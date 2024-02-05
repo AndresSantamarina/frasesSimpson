90074f902a10713f98e71534dd50c2bd1828f9d2
Andres Santamarina <andresesantamarina@gmail.com>
2024-02-05 11:24:37 -0300

    cambie el alt y el className de la imagen

--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -9,7 +9,7 @@ const Frase = ({personaje}) => {
   return (
     <Container className="my-3">
       <h2>{personaje.character}</h2>
-      <Image src={personaje.image} alt="Imagen del personaje" fluid className="bart" />
+      <Image src={personaje.image} alt={personaje.character} fluid className="imagenPersonaje" />
       <p className="text-center my-3">{personaje.quote}</p>
     </Container>
   );
